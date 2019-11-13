cca483916d47243ce2372294f04218a21e613e8f
PierreLansonneur <43953419+PierreLansonneur@users.noreply.github.com>
2019-11-13 08:26:29 +0200

    Update OpenFile.py

--- OpenFile.py
+++ OpenFile.py
@@ -90,8 +90,141 @@ def OpenDicomFile(self, filepath):
 
     self.myCTVolume.open = True
     
-##	Set_axes_lim_init()
-##	Set_scales()
+    #self.Set_axes_lim_init()
+    self.SetScales()
     self.update()
         
+    self.statusBar().showMessage('file successfully opened!')
+    
+def OpenDicomSerie(self, dirname=None):
+    """
+    Open a dicom serie
+    """
+    ct_swapY, ct_swapZ = False, False
+    self.statusBar().showMessage('Opening DICOM serie ... ')
+
+    # Opening file
+    if(dirname==False):
+        filepath, _ = QFileDialog.getOpenFileNames(self, "Open file","./","DICOM (*.dcm)")
+        filepath = filepath[0]
+        filename = QtCore.QFileInfo(filepath[0]).fileName()
+        filedir = QtCore.QFileInfo(filepath[0]).path() # +'/'
+        filelist = os.listdir(os.path.dirname(filepath))
+        #print(filelist)
+        
+    else:
+        filelist = os.listdir(dirname)
+        filepath = dirname + filelist[0]
+
+    #filename_CT = file_path
+    #dir_ini = str(file_path.rsplit('/', 1)[0])+'/'
+
+    # Getting dimensions
+    ds = pydicom.read_file(filepath)
+    sp = ds.PixelSpacing
+    ds.file_meta.TransferSyntaxUID = pydicom.uid.ImplicitVRLittleEndian
+    ct_swapZ =(ds.ImageOrientationPatient[0:3] == [1, 0, 0])
+    ct_swapY =(ds.ImageOrientationPatient[3:6] == [0, 1, 0])
+
+    self.myCTVolume.dim_x = 0
+    for f in filelist:
+        if f.endswith(".dcm"): self.myCTVolume.dim_x += 1        
+
+    self.myCTVolume.dim_y, self.myCTVolume.dim_z = np.shape(ds.pixel_array)[1], np.shape(ds.pixel_array)[0]
+
+    self.myCTVolume.volume = np.zeros((self.myCTVolume.dim_x, self.myCTVolume.dim_y,self.myCTVolume.dim_z))
+    slicelocation = np.zeros(self.myCTVolume.dim_x)
+
+    # creating volume
+    for f,i in zip(filelist,range(self.myCTVolume.dim_x)):
+        if f.endswith(".dcm"):
+            ds = pydicom.read_file(os.path.dirname(filepath)+'/'+f)
+            ds.file_meta.transfersyntaxuid = pydicom.uid.ImplicitVRLittleEndian 
+            self.myCTVolume.volume[i,:,:] = ds.pixel_array
+            if('slicelocation' in ds):  slicelocation[i] = ds.SliceLocation
+            else:   slicelocation[i] = ds.ImagePositionPatient[2]
+
+    order = np.argsort(slicelocation)
+    slicelocation = slicelocation[order] # slicelocation is now sorted
+
+    self.myCTVolume.spacing = [float(slicelocation[1] - slicelocation[0]),float(sp[1]), float(sp[0])]
+    self.myCTVolume.origin = [float(slicelocation[0]),float(ds.ImagePositionPatient[1]),float(ds.ImagePositionPatient[0])]
+    self.myCTVolume.volume = self.myCTVolume.volume[order,:,:] # volume is now sorted
+
+    if ("RescaleSlope" in ds):	self.myCTVolume.volume *= float(ds.RescaleSlope)
+    if ("RescaleIntercept" in ds):	self.myCTVolume.volume += float(ds.RescaleIntercept)
+
+    # Dealing with image orientation
+    print('ct_swapY, ct_swapZ :', ct_swapY, ct_swapZ)
+    
+    if ct_swapY:
+        self.myCTVolume.volume = np.flip(self.myCTVolume.volume,1) # flip volume, Y direction
+        self.myCTVolume.origin[1] += self.myCTVolume.dim_y*self.myCTVolume.spacing[1]
+
+    if ct_swapZ:
+        self.myCTVolume.volume = np.flip(self.myCTVolume.volume,2) # flip volume, Z direction
+        self.myCTVolume.origin[2] += self.myCTVolume.dim_z*self.myCTVolume.spacing[2]
+        
+    if ct_swapZ and ct_swapY:   self.myCTVolume.spacing[1], self.myCTVolume.spacing[2] = self.myCTVolume.spacing[2], self.myCTVolume.spacing[1]
+
+    #self.Set_axes_lim_init()
+    self.SetScales()
+    #CT_open = True
+    self.UpdateAll()
+
+    self.statusBar().showMessage('file successfully opened!')
+
+def OpenDosi(self,filepath=None): 
+    """
+    Open a dosimetry file with a .dcm or .mhd extension
+    """
+
+    dosi_swapY,dosi_swapZ = False, False
+
+    types = [('All files', '*.dcm *.mhd'), ('DCM files', '*.dcm'), ('MHD files', '*.mhd')]
+
+    if(filepath==False):
+        filepath, _ = QFileDialog.getOpenFileNames(self, "Open file","./","DICOM (*.dcm)")
+        filepath = filepath[0]
+        filename = QtCore.QFileInfo(filepath[0]).fileName()
+        filedir = QtCore.QFileInfo(filepath[0]).path() # +'/'
+
+    print('Opening RD file ...')
+
+    ### .dcm file ###
+    if(filepath.endswith('.dcm')):
+        ds = pydicom.read_file(filepath)
+        ds.file_meta.TransferSyntaxUID = pydicom.uid.ImplicitVRLittleEndian 
+        self.myDosiVolume.volume = float(ds.DoseGridScaling)*ds.pixel_array
+        sp = ds.PixelSpacing
+        self.myDosiVolume.spacing = [ float(ds.GridFrameOffsetVector[1] - ds.GridFrameOffsetVector[0]), float(sp[1]),float(sp[0])]
+        self.myDosiVolume.origin = ds.ImagePositionPatient
+        self.myDosiVolume.origin = [float(self.myDosiVolume.origin[2]),float(self.myDosiVolume.origin[1]),float(self.myDosiVolume.origin[0])]
+        dosi_swapZ =(ds.ImageOrientationPatient[0:3] == [1, 0, 0])
+        dosi_swapY =(ds.ImageOrientationPatient[3:6] == [0, 1, 0])
+    
+        self.myDosiVolume.dim_x = np.shape(self.myDosiVolume.volume)[0]
+        self.myDosiVolume.dim_y = np.shape(self.myDosiVolume.volume)[1]        
+        if(len(np.shape(self.myDosiVolume.volume))==3):   self.myDosiVolume.dim_z = np.shape(self.myDosiVolume.volume)[2]
+        if(len(np.shape(self.myDosiVolume.volume))==2):   self.myDosiVolume.dim_z = 1
+
+    #print 'dosi type', dosi.dtype
+    
+    # Dealing with image orientation
+    if(dosi_swapY == True):
+        self.myDosiVolume.volume = np.flip(self.myDosiVolume.volume,1) # flip volume
+        self.myDosiVolume.origin[1] += self.myDosiVolume.dim_y*self.myDosiVolume.spacing[1]		
+    if(dosi_swapZ == True):
+        self.myDosiVolume.volume = np.flip(self.myDosiVolume.volume,2) # flip volume
+        self.myDosiVolume.origin[2] += self.myDosiVolume.dim_z*self.myDosiVolume.spacing[2]
+    if(dosi_swapY == True)and(dosi_swapZ == True):
+        self.myDosiVolume.spacing[1], self.myDosiVolume.spacing[2] = self.myDosiVolume.spacing[2], self.myDosiVolume.spacing[1]
+
+    print('dosi_swapY, dosi_swapZ :', dosi_swapY, dosi_swapZ)
+
+    #dosi_open = True
+    #isodose_show = True
+    #check1.select()
+    self.UpdateAll()
+
     self.statusBar().showMessage('file successfully opened!')
